9104f5333297214b6715a492b69f8747f211376a
kapil kumar <kapilg1996p@gmail.com>
2026-08-18 21:20:55 +0530

    tf Fmt

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
-    azurerm={
-        version = "4.78.0"
-        source = "hashicorp/azurerm"
+    azurerm = {
+      version = "4.78.0"
+      source  = "hashicorp/azurerm"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "kapil21"
+  name     = "kapil21"
   location = "Central India"
 }
